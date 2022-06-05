bfb37370312d5940a6bc64aefe85deeea29022ae
Dieter Haude <dieter@haudeonline.de>
2022-06-05 12:06:16 +0200

    dhaude

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,36 @@
 #include <Arduino.h>
+#include <TaskScheduler.h>
 
-#define dc 1  // shortest duty cycle (1% equ. 2.55)
-#define taster1 4
+// assign Pin's
+#define tmosgate 0
+#define lenable 1 // PWM for marker
 #define led 2
-#define lenable 0 // PWM for marker
-#define minutes 2
-const unsigned long DUR1 = 1000UL*60*minutes;
+#define pwm_signal 3
+#define button 4
+
+// other defines
+#define dc 1                  // duty cycle 1% equ. 2.55
+#define dur2 5 * TASK_MINUTE // Lüfter Nachlauf
+#define dur1 2 * TASK_MINUTE  // Marker Leuchtdauer
 byte status = false;
 volatile int Dac = 0;
 volatile int Cycle = 0;
+
+Scheduler ts;
+
+void task1Callback();
+void task2Callback();
+void task3Callback();
+void pwm_on();
+void pwm_off();
+
+// ==== Task definitions ========================
+Task t1(100 * TASK_MILLISECOND, TASK_FOREVER, &task1Callback, &ts, true);
+Task t2(0,TASK_ONCE, &task2Callback, &ts, false);
+Task t3(0, TASK_ONCE, &task3Callback, &ts, false);
+
+// ==== CODE ======================================================================================
+
 
 // Overflow interrupt
 ISR (TIMER1_OVF_vect) {
@@ -32,20 +54,52 @@ void analogWrite12 (int value) {
         sei();
 }
 
-void setup() {
-        pinMode(taster1, INPUT_PULLUP);
-        pinMode(led, OUTPUT);
-        digitalWrite(lenable, LOW);
-        digitalWrite(led, LOW);
+void setup()
+{
+  pinMode(pwm_signal, INPUT);
+  pinMode(button, INPUT_PULLUP);
+  pinMode(lenable, OUTPUT);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+  digitalWrite(lenable, LOW);
+  // Top value for high (Table 12-2)
+  OCR1C = 255;
+  // Timer/Counter1 doing PWM on OC1A (PB1)
+  TCCR1 = 1 << PWM1A    // Pulse Width Modulator A Enable
+          | 1 << COM1A0 // OC1x cleared on compare match. Set when TCNT1 = $00
+          | 1 << CS10;  // PWM clock = CK
+  TIMSK |= 1 << TOIE1;  // Timer/Counter1 Overflow Interrupt Enable
+}
 
-        // Top value for high (Table 12-2)
-        OCR1C = 255;
-        // Timer/Counter1 doing PWM on OC1A (PB1)
-        TCCR1 = 1 << PWM1A    // Pulse Width Modulator A Enable
-                | 1 << COM1A0 // OC1x cleared on compare match. Set when TCNT1 = $00
-                | 1 << CS10;  // PWM clock = CK
-        TIMSK |= 1 << TOIE1; // Timer/Counter1 Overflow Interrupt Enable
-        pinMode(1, OUTPUT);
+void loop()
+{
+  ts.execute();
+}
+
+void task1Callback()
+{
+  if (digitalRead(pwm_signal)){
+    t1.disable();
+    digitalWrite(tmosgate, HIGH);
+    t2.restartDelayed(dur2);    // Lüfter Nachlauf
+  } else {
+    if ((digitalRead(led) == LOW) and (digitalRead(button) == LOW))
+    {
+      pwm_on();
+      t3.enableDelayed(dur1);   // Dauer Laser Low Energy on
+    }
+  }
+}
+
+void task2Callback()
+{
+  digitalWrite(tmosgate, LOW);
+  t1.enable();
+}
+
+void task3Callback()
+{
+  pwm_off();
 }
 
 void pwm_on()
@@ -58,18 +112,4 @@ void pwm_off()
 {
         analogWrite12(0);
         digitalWrite(led, LOW);
-}
-
-void loop() {
-        if(digitalRead(taster1) == LOW)
-        {
-                status = !status;
-                while(digitalRead(taster1) == LOW);
-                delay(20);         // keeps a small delay
-                if (status == true) {
-                        pwm_on();
-                } else {
-                        pwm_off();
-                }
-        }
 }
